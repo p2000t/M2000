8f9036287ba442e7e3daadb4b7e9860c20043fc8
Dion Olsthoorn <dion.olsthoorn@gmail.com>
2023-10-17 11:44:57 +0200

    Removed superfluous files

--- BootFromTape.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if "%~1"=="" (
-  echo Drag-and-drop a .cas program file on 'BootFromTape.bat' to start the program in the M2000 emulator directly
-  pause&exit
-)
-cd %~dp0
-m2000.exe -ram 64k -tape "%~1" -boot 1 -video 1
