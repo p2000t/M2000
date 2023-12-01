c9d42c2c4718704335e8b399aa8d0400e0c0e2c7
Dion Olsthoorn <dion.olsthoorn@gmail.com>
2023-12-01 13:00:51 +0100

    msys2 build fix

--- package/WiX/build32.bat
+++ package/WiX/build32.bat
@@ -1,2 +1,2 @@
-"%WIX%bin\candle" -arch x86 product.wxs
-"%WIX%bin\light" -ext WixUIExtension -ext WixUtilExtension -sacl -spdb -out M2000-installer.msi product.wixobj
+"%WIX%bin\candle" -arch x86 product32.wxs
+"%WIX%bin\light" -ext WixUIExtension -ext WixUtilExtension -sacl -spdb -out M2000-installer.msi product32.wixobj
