b7ae2b634abbe42af65b4767335d7f74f69c5f72
dionoid <dion.olsthoorn@gmail.com>
2024-06-04 21:51:05 +0200

    bump to version 0.9.2

--- src/libretro/m2000_libretro.c
+++ src/libretro/m2000_libretro.c
@@ -470,7 +470,7 @@ void retro_get_system_info(struct retro_system_info *info)
 {
    memset(info, 0, sizeof(*info));
    info->library_name     = "M2000";
-   info->library_version  = "v0.9.1";
+   info->library_version  = "v0.9.2";
    info->need_fullpath    = true;
    info->valid_extensions = "cas|p2000t";
 }
